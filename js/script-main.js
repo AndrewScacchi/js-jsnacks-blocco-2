f3861158b4d2e9664efedfdace690fde0ae365e5
andrea scacchi <andrea.scacchi@live.com>
2022-05-11 17:13:09 +0200

    Completed exercise adding multiple extra functions to test basic level dom manipulation

--- js/script-main.js
+++ js/script-main.js
@@ -1,1 +1,38 @@
 // Script for main
+
+// dichiaro un array
+const allUsers = [];
+const maxTotalUsers = parseInt(prompt("Select a maximun users number!"));
+let totalUsers = 0;
+const pageId = document.getElementById("maxUsers");
+pageId.append(maxTotalUsers);
+
+alert(`Maximum total users number is set to ${maxTotalUsers}`);
+
+while(totalUsers < maxTotalUsers){
+    let newUserNum = parseInt(prompt("Add some users Man, is empty here!"));
+    totalUsers += newUserNum;
+
+    if (totalUsers == maxTotalUsers){
+        alert(`
+                Maximum number of ${maxTotalUsers} users has been reached!
+                GoodBye!
+            `);
+    } else if (totalUsers <= maxTotalUsers){
+        allUsers.push(newUserNum);
+        alert(`There are new a total of ${totalUsers} users!`);
+    } else {
+        let tooManyUsers = totalUsers;
+        alert(` 
+                There are now ${tooManyUsers} users, they are too many!
+                Only max ${maxTotalUsers} are allowed.
+                Last allowed number is ${totalUsers - newUserNum}
+            `);
+    }
+    
+    
+}
+
+
+
+
